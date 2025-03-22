0ecfb6e487ca63422a0c1501673a254c6d6d5a94
kashtan1100 <61379997+kashtan1100@users.noreply.github.com>
2025-03-22 18:44:25 +0300

    wip

--- dist/assets/swipe-back-legacy-4haYGxg6.js
+++ dist/assets/swipe-back-legacy-4haYGxg6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CQDA4mDn.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.$,r=t.a0,i=t.a1}],execute:function(){
+System.register(["./index-legacy-BeTAZiAz.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.$,r=t.a0,i=t.a1}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     vue(),
     legacy()
   ],
-  base: '/tinder/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
