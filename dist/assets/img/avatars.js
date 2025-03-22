623289982b88d7ae2c3eb8fd114ca77b78e80c16
kashtan1100 <61379997+kashtan1100@users.noreply.github.com>
2025-03-22 18:18:46 +0300

    kuku

---
+++ dist/assets/img/avatars.js
@@ -0,0 +1,23 @@
+export default const USERS = [
+  {
+    id: 1,
+    name: 'Black Panther',
+    profileUrl: 'assets/img/avatars/tanya1.png',
+  },
+  {
+    id: 2,
+    name: 'Black Widow',
+    profileUrl: 'assets/img/avatars/tanya2.png',
+  },
+  {
+    id: 3,
+    name: 'Captain',
+    profileUrl: 'assets/img/avatars/tanya.png',
+  },
+  {
+    id: 4,
+    name: 'Hieu Pham',
+    profileUrl: 'assets/img/avatars/tanya3.png',
+  },
+  
+]
